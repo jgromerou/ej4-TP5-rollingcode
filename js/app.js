665fe0ce0f716edc20cba41c2cf76ba609a100c3
Juan Gerardo Romero Uro <jgromerouro@gmail.com>
2023-04-11 08:33:18 -0300

    se está haciendo la logica de la hora de otras ciudades

--- js/app.js
+++ js/app.js
@@ -1,4 +1,8 @@
+mostrarHora();
 setInterval(mostrarHora, 1000); //guardo el identificador unico del setInterval
+
+//TODO: Traer region para hacer un addEventListener
+
 // $(function () {
 //   mostrarHora();
 //   setInterval(mostrarHora, 1000);
@@ -73,39 +77,54 @@ function mostrarHora() {
 function horaRegion() {
   //Hora Regiones
 
-  var regiones = [
-    { region: 'México DC', diferencia: 0 },
-    { region: 'Sao Paulo', diferencia: 2 },
+  let regiones = [
+    { region: 'México DC', diferencia: -3 },
+    { region: 'Sao Paulo', diferencia: 0 },
     { region: 'Washington DC', diferencia: -1 },
-    { region: 'Madrid', diferencia: 7 },
-    { region: 'Seúl', diferencia: 14 },
-    { region: 'Toronto', diferencia: 1 },
-    { region: 'Bogotá', diferencia: 0 },
-    { region: 'Caracas', diferencia: 1 },
+    { region: 'Madrid', diferencia: 5 },
+    { region: 'Seúl', diferencia: 12 },
+    { region: 'Toronto', diferencia: -1 },
+    { region: 'Bogotá', diferencia: -2 },
+    { region: 'Caracas', diferencia: -1 },
   ];
-  var fecha = new Date();
-  var checked = this.checked;
-  var index = parseInt(this.value);
+  let fechaActual = new Date();
+  let checked = this.checked;
+  let index = parseInt(this.value);
   if (checked) {
-    hora = fecha.getHours() + regiones[index].diferencia;
+    hora = fechaActual.getHours() + regiones[index].diferencia;
     if (hora >= 24) hora = hora - 24;
     if (hora < 10) hora = '0' + hora;
-    minutos = fecha.getMinutes();
+    minutos = fechaActual.getMinutes();
     if (minutos < 10) minutos = '0' + minutos;
-    segundos = fecha.getSeconds();
+    segundos = fechaActual.getSeconds();
     if (segundos < 10) segundos = '0' + segundos;
-    $('#otros').append(
-      "<div class='row" +
-        index +
-        "'><div class='col-xs-6'><span class='ciudad'>" +
-        regiones[index].region +
-        "</span></div><div class='col-xs-6'><span class='hora-ciudad'>" +
-        hora +
-        ':' +
-        minutos +
-        ':' +
-        segundos +
-        '</span></div></div>'
-    );
+
+    let otros = document.getElementsById('otros');
+
+    otros.append(`<div class='row" 
+    ${index} +
+    "'><div class='col-xs-6'><span class='ciudad'>" +
+    ${regiones[index].region} +
+    "</span></div><div class='col-xs-6'><span class='hora-ciudad'>" +
+    ${hora} +
+    ':' +
+    ${minutos} +
+    ':' +
+    ${segundos} +
+    '</span></div></div>`);
+
+    // $('#otros').append(
+    //   "<div class='row" +
+    //     index +
+    //     "'><div class='col-xs-6'><span class='ciudad'>" +
+    //     regiones[index].region +
+    //     "</span></div><div class='col-xs-6'><span class='hora-ciudad'>" +
+    //     hora +
+    //     ':' +
+    //     minutos +
+    //     ':' +
+    //     segundos +
+    //     '</span></div></div>'
+    // );
   } else $('.row' + index).remove();
 }
