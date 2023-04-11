5e9280927a450c9a83cf99fc61aa7d60796b80d7
Juan Gerardo Romero Uro <jgromerouro@gmail.com>
2023-04-11 08:23:39 -0300

    se agregó fecha y hora local distinguiendo en horas AM y PM, se agregó el formato 00:00:00 en la hora y haciendo la logica de otros lugares

--- js/app.js
+++ js/app.js
@@ -1,48 +1,111 @@
-let formTextarea = document.getElementById('formTextarea');
-let cabeceraTabla = document.getElementById('cabeceraTabla');
-let contenidoTabla = document.getElementById('contenidoTabla');
-let cadenaTabla = [];
+setInterval(mostrarHora, 1000); //guardo el identificador unico del setInterval
+// $(function () {
+//   mostrarHora();
+//   setInterval(mostrarHora, 1000);
+//   $('.region').on('click', horaRegion);
+// });
 
-//activadores de eventos
-formTextarea.addEventListener('submit', agregarTarea);
+function mostrarHora() {
+  //Hora local
+  let fechaActual = new Date();
+  const semana = [
+    'Domingo',
+    'Lunes',
+    'Martes',
+    'Miércoles',
+    'Jueves',
+    'Viernes',
+    'Sábado',
+  ];
+  const meses = [
+    'Enero',
+    'Febrero',
+    'Marzo',
+    'Abril',
+    'Mayo',
+    'Junio',
+    'Julio',
+    'Agosto',
+    'Septiembre',
+    'Octubre',
+    'Noviembre',
+    'Diciembre',
+  ];
 
-function agregarTarea(tarea) {
-  tarea.preventDefault();
+  //formato 00:00:00
+  let segundos = fechaActual.getSeconds();
+  if (segundos < 10) segundos = '0' + segundos;
+  let minutos = fechaActual.getMinutes();
+  if (minutos < 10) minutos = '0' + minutos;
+  let horas = fechaActual.getHours();
+  if (horas < 10) horas = '0' + horas;
 
-  //se muestra la cabecera de la tabla
-  cabeceraTabla.classList.remove('d-none');
+  //parrafo fecha
+  let pFecha = document.getElementById('fecha');
+  pFecha.innerHTML = `${
+    semana[fechaActual.getDay()]
+  } ${fechaActual.getDate()} de ${
+    meses[fechaActual.getMonth()]
+  } del ${fechaActual.getFullYear()}`;
 
-  //valida si ingresó una cadena
-  if (tarea.target[0].value.length > 0) {
-    //se hace un push a la cadenaTabla
-    cadenaTabla.push(tarea.target[0].value);
+  //parrafo de la hora
+  let pHora = document.getElementById('hora');
 
-    //Crear th
-    let th = document.createElement('th');
-    //th.scope = 'row';
-    th.innerText = cadenaTabla.length;
-
-    //crear td tarea
-    let tdTarea = document.createElement('td');
-    tdTarea.innerText = tarea.target[0].value;
-
-    //crear td boton eliminar
-    let tdBotonEliminar = document.createElement('td');
-    tdBotonEliminar.innerHTML = `<i class="bi bi-trash3-fill"></i>`;
-
-    //Crear tr
-    let tr = document.createElement('tr');
-    tr.appendChild(th);
-    tr.appendChild(tdTarea);
-    tr.appendChild(tdBotonEliminar);
-
-    //se agrega el tr al contenido de la tabla
-    contenidoTabla.append(tr);
-
-    //Reset el textarea
-    tarea.target[0].value = '';
+  if (horas >= 12) {
+    pHora.innerHTML = `${horas % 12}: ${minutos}: ${segundos} PM`;
   } else {
-    alert('Por favor, ingrese una cadena con la tarea');
-    tarea.target[0].value = '';
+    pHora.innerHTML = `${horas}: ${minutos}: ${segundos} AM`;
   }
+
+  // let fecha = new Date();
+  // let segundos = fecha.getSeconds();
+  // if (segundos < 10) segundos = '0' + segundos;
+  // let minutos = fecha.getMinutes();
+  // if (minutos < 10) minutos = '0' + minutos;
+  // let horas = fecha.getHours();
+  // if (horas < 10) horas = '0' + horas;
+
+  // $('#hora').html(horas + ':' + minutos + ':' + segundos);
+  // $('#dia').html(semana[fecha.getDay()] + ', ');
+  // $('#fecha').html(fecha.getDate() + ' de ' + meses[fecha.getMonth()]);
+}
+
+function horaRegion() {
+  //Hora Regiones
+
+  var regiones = [
+    { region: 'México DC', diferencia: 0 },
+    { region: 'Sao Paulo', diferencia: 2 },
+    { region: 'Washington DC', diferencia: -1 },
+    { region: 'Madrid', diferencia: 7 },
+    { region: 'Seúl', diferencia: 14 },
+    { region: 'Toronto', diferencia: 1 },
+    { region: 'Bogotá', diferencia: 0 },
+    { region: 'Caracas', diferencia: 1 },
+  ];
+  var fecha = new Date();
+  var checked = this.checked;
+  var index = parseInt(this.value);
+  if (checked) {
+    hora = fecha.getHours() + regiones[index].diferencia;
+    if (hora >= 24) hora = hora - 24;
+    if (hora < 10) hora = '0' + hora;
+    minutos = fecha.getMinutes();
+    if (minutos < 10) minutos = '0' + minutos;
+    segundos = fecha.getSeconds();
+    if (segundos < 10) segundos = '0' + segundos;
+    $('#otros').append(
+      "<div class='row" +
+        index +
+        "'><div class='col-xs-6'><span class='ciudad'>" +
+        regiones[index].region +
+        "</span></div><div class='col-xs-6'><span class='hora-ciudad'>" +
+        hora +
+        ':' +
+        minutos +
+        ':' +
+        segundos +
+        '</span></div></div>'
+    );
+  } else $('.row' + index).remove();
 }
